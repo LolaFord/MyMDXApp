ee4305ed54a09af6cf01d821cecb330544f897d0
LolaFord <mrobofl@gmail.com>
2016-12-25 21:10:33 -0500

    Added initial files

---
+++ MyMvc3App/Controllers/MyMDXController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMvc3App.Controllers
+{
+    public class MyMDXController : Controller
+    {
+        //
+        // GET: /MyMDX/
+
+        ///public ActionResult Index()
+        //{
+            //return View();
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public string Welcome(string name, int numTimes = 1)
+        {
+            string message = "Hello " + name + ", numTimes is: " + numTimes;
+            return "" + Server.HtmlEncode(message) + "";
+        }
+    }
+}
